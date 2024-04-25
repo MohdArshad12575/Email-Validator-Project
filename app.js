5d4bb5abe5e0fc4f70504ba715b7c195da98156a
Mohd Arshad <mohdarshad1575@gmail.com>
2024-04-25 00:28:05 -0700

    js done

--- app.js
+++ app.js
@@ -2,28 +2,29 @@ let btn = document.querySelector(".submit-btn");
 let img = document.querySelector("#loading-icon");
 let inp = document.querySelector("input");
 
-let url =
-  "https://api.emailvalidation.io/v1/info?apikey=ema_live_gLg1lVHpXae4id0AgoGJXI1Zk0RPCZ8DSv30U1hx&email=";
-
 btn.addEventListener("click", async (e) => {
   e.preventDefault();
   img.src = "img/loading.svg";
   getData();
 });
 
+let url =
+  "https://api.emailvalidation.io/v1/info?apikey=ema_live_gLg1lVHpXae4id0AgoGJXI1Zk0RPCZ8DSv30U1hx&email=";
+
 async function getData() {
   let email = inp.value;
   let data = await fetch(url + email);
   let finaldata = await data.json();
   img.src = "";
-  let str = ``;
+
+  let resultcont = document.querySelector(".results");
+  resultcont.innerHTML = "";
   for (key of Object.keys(finaldata)) {
     if (finaldata[key] !== "" && finaldata[key] !== " ") {
-      str = str + `${key} : ${finaldata[key]}`;
+      // Check if value is not empty or whitespace
+      let paragraph = document.createElement("p");
+      paragraph.textContent = `${key} : ${finaldata[key]}`;
+      resultcont.appendChild(paragraph);
     }
   }
-  let h5 = document.createElement("h5");
-  let res = document.querySelector(".result-sec");
-  res.appendChild(h5);
-  h5.innerText = str;
 }
